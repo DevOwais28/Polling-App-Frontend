39293a0a01a70289e578974fd3e1aa90b5f0326b
Owais Ahmed <153555180+DevOwais28@users.noreply.github.com>
2025-11-20 16:27:17 +0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import useAppStore from "./store";
 
 const api = axios.create({
-    baseURL: "https://polling-app-frontend-coral.vercel.app/api/",
-});
+    baseURL: "https://polling-app-production-142d.up.railway.app/api/"});
 
 api.interceptors.request.use((config) => {
     const token = useAppStore.getState().token
